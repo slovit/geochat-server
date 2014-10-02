c73d9e520214c0101dc8a69aeb2410d6067b8bed
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2014-10-02 14:19:53 -0400

    Integrated Swagger

--- src/main/java/ca/cencol/geochat/model/IncommingMessage.java
+++ src/main/java/ca/cencol/geochat/model/IncommingMessage.java
@@ -1,8 +1,12 @@
 package ca.cencol.geochat.model;
 
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
 
+@ApiModel(value = "A message posted by the user")
 public class IncommingMessage {
   
+  @ApiModelProperty(value = "Message text", required = true, allowableValues = "Non-empty string")
   private String message;
   
   public IncommingMessage() {
--- src/main/java/ca/cencol/geochat/model/Message.java
+++ src/main/java/ca/cencol/geochat/model/Message.java
@@ -2,11 +2,19 @@ package ca.cencol.geochat.model;
 
 import java.util.Date;
 
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "A single chat message returned by the API")
 public class Message {
 
+  @ApiModelProperty(value = "Unique user identifier", required = true)
   private final String userId;
+  @ApiModelProperty(value = "User's nickname", required = true)
   private final String username;
+  @ApiModelProperty(value = "When the message was posted", required = true)
   private final Date time;
+  @ApiModelProperty(value = "Message text", required = true, allowableValues = "Non-empty string")
   private final String message;
 
   public Message(String userId, String username, String message) {
@@ -31,7 +39,7 @@ public class Message {
   public String getMessage() {
     return message;
   }
-  
+
   @Override
   public String toString() {
     return String.format("UserId: %s", userId);
